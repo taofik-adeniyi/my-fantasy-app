9d28e84e3fc8c95f77a85f18a21540da6e2815e2
taofik-adeniyi <bidemi64@gmail.com>
2021-06-26 10:32:29 +0100

    setup on main

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,43 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { Text, View, ImageBackground, StyleSheet, SafeAreaView } from 'react-native';
+import field from '../assets/images/field.jpg';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-
+const players: {[key: string]: null[]} = {
+  FWD: [null, null, null],
+  MID: [null, null, null],
+  DEF: [null, null, null, null],
+  GKC: [null],
+}
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+    <SafeAreaView style={styles.container}>
+       <ImageBackground 
+        source={field}
+        resizeMode="contain"
+        style={styles.bgimage}
+        >
+          {Object.keys(players).map((position,id)=>(
+              <View key={id} style={{flexDirection: 'row', width: '100%', justifyContent: 'space-around'}}>
+                {players[position].map((player,id)=>(
+                  <Text key={id}>{position}</Text>
+                ))}
+              </View>
+          ))}
+      </ImageBackground>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'center'
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  bgimage: {
+    width: '100%',
+    height: '100%',
+    aspectRatio: 2/3,
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  }
 });
